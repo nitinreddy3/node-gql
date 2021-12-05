16a7acc0b2eb2faea7edee19dacdf9f2d0d67162
Nitin Reddy <reddynitin3@gmail.com>
2021-12-06 00:05:22 +0530

    Created a simple graphql instance

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const resolver = {
+  hello: () => 'Hello world!',
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: resolver,
+  graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
