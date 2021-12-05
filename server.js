43306a3436a91b20c05b822dd81870b4d8f4f155
Nitin Reddy <reddynitin3@gmail.com>
2021-12-06 00:25:24 +0530

    Updated the schema and the resolver

--- server.js
+++ server.js
@@ -5,13 +5,49 @@ import { buildSchema } from 'graphql';
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
+    user(id: Int!): Person
+    users(shark: String): [Person]
+  }
+  type Person {
+    id: Int
+    name: String
+    age: Int
+    shark: String
   }
 `);
 
+const users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: '21',
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: '22',
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: '23',
+    shark: 'Hammerhead Shark'
+  }
+];
+
+//Returns a single user
+const getUser = ({ id }) => users.filter(user => user.id === id)[0];
+
+//Returns a list of users
+const retrieveUsers = ({ shark }) => shark ? users.filter(user => user.shark === shark) : users;
+
+
 //Construct a resolver function
 const resolver = {
-  hello: () => 'Hello world!',
+  user: getUser,
+  users: retrieveUsers
 };
 
 const app = express();
